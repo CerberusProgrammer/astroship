dc6ef9754a6cf1460f435919a8bb8d88f5a35180
sazar <sazardev@gmail.com>
2024-11-24 08:08:35 -0800

    Add Spaceship class to manage movement and shooting mechanics

---
+++ src/scripts/spaceship.ts
@@ -0,0 +1,100 @@
+import type { Bullet } from "./bullet";
+
+export class Spaceship {
+  element: HTMLDivElement;
+  x: number;
+  speed: number;
+  bulletSpeed: number;
+  bullets: Bullet[] = [];
+  bulletPool: Bullet[] = [];
+  keys: { [key: string]: boolean } = {};
+
+  constructor(private gameContainer: HTMLDivElement) {
+    this.element = document.getElementById("spaceship") as HTMLDivElement;
+    this.x = gameContainer.clientWidth / 2;
+    this.speed = 5;
+    this.bulletSpeed = 15;
+
+    document.addEventListener("keydown", (event) => this.handleKeyDown(event));
+    document.addEventListener("keyup", (event) => this.handleKeyUp(event));
+  }
+
+  move() {
+    this.element.style.left = `${this.x}px`;
+  }
+
+  shoot() {
+    const bullet = this.getBullet();
+    bullet.style.left = `${this.x - 5}px`;
+    bullet.style.bottom = "150px";
+    this.bullets.push(bullet);
+  }
+
+  updateBullets() {
+    this.bullets.forEach((bullet, index) => {
+      const bulletBottom = parseInt(bullet.style.bottom);
+      if (bulletBottom > this.gameContainer.clientHeight) {
+        bullet.style.display = "none";
+        this.bullets.splice(index, 1);
+        this.bulletPool.push(bullet);
+      } else {
+        bullet.style.bottom = `${bulletBottom + this.bulletSpeed}px`;
+      }
+    });
+  }
+
+  private getBullet(): Bullet {
+    let bullet: Bullet;
+    if (this.bulletPool.length > 0) {
+      bullet = this.bulletPool.pop()!;
+      bullet.style.display = "block";
+    } else {
+      bullet = document.createElement("div") as Bullet;
+      bullet.classList.add("bullet");
+      this.gameContainer.appendChild(bullet);
+    }
+    return bullet;
+  }
+
+  private handleKeyDown(event: KeyboardEvent) {
+    this.keys[event.key] = true;
+    if (event.key === " ") {
+      this.shoot();
+    }
+  }
+
+  private handleKeyUp(event: KeyboardEvent) {
+    this.keys[event.key] = false;
+  }
+
+  updatePosition() {
+    let isMoving = false;
+    if (this.keys["a"] || this.keys["A"]) {
+      this.x = Math.max(0, this.x - this.speed);
+      this.element.style.transform = `translate(-50%, -50%) rotate(-10deg)`;
+      isMoving = true;
+    }
+    if (this.keys["d"] || this.keys["D"]) {
+      this.x = Math.min(
+        this.gameContainer.clientWidth - 40,
+        this.x + this.speed
+      );
+      this.element.style.transform = `translate(-50%, -50%) rotate(10deg)`;
+      isMoving = true;
+    }
+    if (
+      !this.keys["a"] &&
+      !this.keys["A"] &&
+      !this.keys["d"] &&
+      !this.keys["D"]
+    ) {
+      this.element.style.transform = `translate(-50%, -50%) rotate(0deg)`;
+    }
+    if (isMoving) {
+      this.element.classList.add("moving");
+    } else {
+      this.element.classList.remove("moving");
+    }
+    this.move();
+  }
+}
